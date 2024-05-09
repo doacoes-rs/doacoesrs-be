96883bc41f1e5d83188669d260a896fd53f5ac90
Raphael Rossi <raphael.rossi@stone.com.br>
2024-05-08 21:35:08 -0300

    feat: added cors validation

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import locations
 
 app = FastAPI()
 
+origins = [
+    "http://doacoesrs.com.br",
+    "https://doacoesrs.com.br",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(locations.router, prefix="/locations")
+
